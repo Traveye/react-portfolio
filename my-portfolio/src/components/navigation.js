a9cd85fd1aea13bd4ecf2ec8666ed27de6c62ade
Big Trees <traveye@protonmail.com>
2023-03-06 16:03:58 -0800

    initial files and adding navigation

---
+++ my-portfolio/src/components/navigation.js
@@ -0,0 +1,18 @@
+//this is for the navigation bar
+
+function Navigation() {
+    return (
+        <div className="Navigation">
+        <nav>
+            <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Projects</a></li>
+            <li><a href="#">Contact</a></li>
+            </ul>
+        </nav>
+        </div>
+    );
+}
+
+export default Navigation;
