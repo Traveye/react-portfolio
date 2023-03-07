bf997bc565915ac8810d0d71d29f1813c59e964c
Big Trees <traveye@protonmail.com>
2023-03-06 17:07:27 -0800

    adding css to nav bar

--- my-portfolio/src/components/navigation.js
+++ my-portfolio/src/components/navigation.js
@@ -1,18 +1,38 @@
 //this is for the navigation bar
 
+// WHEN I view the header
+// THEN I am presented with the developer's name and navigation with titles corresponding to different sections of the portfolio
+// WHEN I view the navigation titles
+// THEN I am presented with the titles About Me, Portfolio, Contact, and Resume, and the title corresponding to the current section is highlighted
+// WHEN I click on a navigation title
+// THEN I am presented with the corresponding section below the navigation without the page reloading and that title is highlighted
+
+import React from 'react';
+import styles from './navigation.module.css';
+
 function Navigation() {
     return (
-        <div className="Navigation">
-        <nav>
-            <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Projects</a></li>
-            <li><a href="#">Contact</a></li>
-            </ul>
-        </nav>
-        </div>
+        <header className={styles.navParent}>
+            <h1>Travis DuPree</h1>
+            <nav>
+                <ul className={styles.ulParent}>
+                    <li>
+                        <a href="#about">About Me</a>
+                    </li>
+                    <li>
+                        <a href="#portfolio">Portfolio</a>
+                    </li>
+                    <li>
+                        <a href="#contact">Contact</a>
+                    </li>
+                    <li>
+                        <a href="#resume">Resume</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
     );
 }
+
 
 export default Navigation;
