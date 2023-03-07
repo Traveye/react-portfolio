e7565071f0b87be20f5d40ee9f0e3d6824847417
Big Trees <traveye@protonmail.com>
2023-03-06 17:47:35 -0800

    adding animation to nav bar and media queries

--- my-portfolio/src/components/navigation.js
+++ my-portfolio/src/components/navigation.js
@@ -7,32 +7,44 @@
 // WHEN I click on a navigation title
 // THEN I am presented with the corresponding section below the navigation without the page reloading and that title is highlighted
 
-import React from 'react';
-import styles from './navigation.module.css';
+import React, { useEffect } from "react";
+import styles from "./navigation.module.css";
+import anime from "animejs/lib/anime.es.js";
 
 function Navigation() {
-    return (
-        <header className={styles.navParent}>
-            <h1>Travis DuPree</h1>
-            <nav>
-                <ul className={styles.ulParent}>
-                    <li>
-                        <a href="#about">About Me</a>
-                    </li>
-                    <li>
-                        <a href="#portfolio">Portfolio</a>
-                    </li>
-                    <li>
-                        <a href="#contact">Contact</a>
-                    </li>
-                    <li>
-                        <a href="#resume">Resume</a>
-                    </li>
-                </ul>
-            </nav>
-        </header>
-    );
+  useEffect(() => {
+    console.log("useEffect ran");
+    anime({
+      targets: "header",
+      translateY: [-100, 0],
+      duration: 1500,
+      easing: "easeInOutSine",
+    });
+    console.log("anime ran");
+  }, []);
+
+  return (
+    <header className={styles.navParent}>
+      <h1>Travis DuPree</h1>
+      <nav>
+        <ul className={styles.ulParent}>
+          <li>
+            <a href="#about">About Me</a>
+          </li>
+          <li>
+            <a href="#portfolio">Portfolio</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+          <li>
+            <a href="#resume">Resume</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
-
 export default Navigation;
+
